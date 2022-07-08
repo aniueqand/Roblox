7b6cb03e6bcf73e50cad519ff09bc3ab0849f964
aniueqand <88556513+aniueqand@users.noreply.github.com>
2022-07-08 21:43:27 +0300

    Delete Pressure-Wash.lua

--- Random Stuff/Pressure-Wash.lua
+++
@@ -1,6 +0,0 @@
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/juywvm/ui-libs/main/unnamed_Notify_Library/NotifyLibrary", true))()
-
-local args = {[1] = 28754544258117.676,[2] = math.huge }
-game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(unpack(args))
-
-Notification.new("success", "Inf Money", "Made by aniueqand")
