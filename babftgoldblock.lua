6abd30e2fa7aeaef4a6720a5e0814c7f142e2296
aniueqand <88556513+aniueqand@users.noreply.github.com>
2022-07-08 23:41:28 +0300

    Update babftgoldblock.lua

--- babftgoldblock.lua
+++ babftgoldblock.lua
@@ -1,42 +1,48 @@
+
+getgenv().Busy = true
+
 --// Services
 local players = game:GetService("Players")
-
+ 
 --// Workspace
 local stages = workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")
-local penguin = workspace:WaitForChild("ChangeCharacter")
-
+local penguin, gold = workspace:WaitForChild("ChangeCharacter"), workspace:WaitForChild("ClaimRiverResultsGold")
+ 
 --// Other
 local client = players.LocalPlayer
-    
+ 
 --// Main
 ---------
 
-while _G.Busy do
+while getgenv().Busy do
     local bodyVelocity = Instance.new("BodyVelocity")
     bodyVelocity.Velocity = Vector3.new(0, -4, 0)
     bodyVelocity.Parent = client.Character.HumanoidRootPart
     
-    for i = 1, 5 do
+    for i = 1, 9 do
         if not client.Character or not client.Character:FindFirstChild("Humanoid") then
             repeat wait() until client.Character and client.Character:FindFirstChild("Humanoid")
         end
-
-        client.Character.HumanoidRootPart.CFrame = stages["CaveStage"..i].DarknessPart.CFrame
+ 
+        client.Character.HumanoidRootPart.CFrame = stages["CaveStage"..i].DarknessPart.CFrame wait(0.1)
         
-        wait(1.2)
+        if not getgenv().Busy then
+            client.Character.Humanoid.Health = 0
+            exit(0)
+        end
+        
+        if i == 1 then
+            wait(4)
+        else
+            wait(2)
+        end
+        
+        gold:FireServer()
     end
-
-    if not client.Character or not client.Character:FindFirstChild("HumanoidRootPart") then
-        repeat wait() until client.Character and client.Character:FindFirstChild("HumanoidRootPart")
-    end
-
-    client.Character.HumanoidRootPart.CFrame = stages["TheEnd"].GoldenChest.Trigger.CFrame
-    
-    wait(10)
-    
+ 
     penguin:FireServer("PenguinCharacter")
     client.Character:Remove()
-    
+ 
     repeat wait() 
     until client.Character and client.Character:FindFirstChild("HumanoidRootPart")
     
