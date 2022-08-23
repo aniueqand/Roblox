56c5cca630463dda13ff12bd494139cf883d3de2
aniueqand <88556513+aniueqand@users.noreply.github.com>
2022-08-23 18:29:00 +0300

    Update and rename Pressure-Wash-Simulator to Pressure-Wash-Simulator.lua

---
+++ Random Stuff/Pressure-Wash-Simulator.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 7009799230 then
+
+local args = {[1] = 28754544258117.676,[2] = math.huge }
+game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(unpack(args))
+print("ez money")
+end
