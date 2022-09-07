5f6d8f08ecc5b9035bc825d09d9fb813795bfa71
aniueqand <88556513+aniueqand@users.noreply.github.com>
2022-09-07 13:15:12 +0300

    Update Loader.lua

--- Hub/Loader.lua
+++ Hub/Loader.lua
@@ -1,10 +1,7 @@
-
 local hubLoader = {
-2316994223 = "https://raw.githubusercontent.com/aniueqand/Roblox/main/Hub/PetSimulatorX.lua",
-210851291 = "https://raw.githubusercontent.com/aniueqand/Roblox/main/Hub/babft.lua"
+  [2316994223] = "https://raw.githubusercontent.com/aniueqand/Roblox/main/Hub/PetSimulatorX.lua",
+  [210851291] = "https://raw.githubusercontent.com/aniueqand/Roblox/main/Hub/babft.lua"
 }
-for i,v in pairs(hubLoader) do
-  if game.GameId == i then
-    loadstring(game:HttpGet(i))()
-  end  
-end    
+
+
+loadstring(game:HttpGet(hubLoader[game.GameId]))()
