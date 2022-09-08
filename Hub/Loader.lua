2f3c0d739bd6e5956d8114f43c316c4005636682
aniueqand <88556513+aniueqand@users.noreply.github.com>
2022-09-08 20:16:43 +0300

    Update Loader.lua

--- Hub/Loader.lua
+++ Hub/Loader.lua
@@ -2,7 +2,6 @@ local hubLoader = {
   [2316994223] = "https://raw.githubusercontent.com/aniueqand/Roblox/main/Hub/PetSimulatorX.lua",
   [210851291] = "https://raw.githubusercontent.com/aniueqand/Roblox/main/Hub/babft.lua"
 }
-
-writefile("aniueqand/HubLoader.UrMom","HubLoadedId-"..math.random(1000, 25000),math.random(1000, 25000),math.random(1000, 25000))
-getgenv().HubLoaded = readfile("aniueqand/HubLoader.UrMom")
+getgenv().aniueqand = {HubLoaded,babft,psx}
+getgenv()aniueqand.HubLoaded = true
 loadstring(game:HttpGet(hubLoader[game.GameId]))()
