e79bcdbef40d25dfa06585ab2bd3a9d37ca97ace
aniueqand <88556513+aniueqand@users.noreply.github.com>
2022-09-08 20:11:45 +0300

    Update Loader.lua

--- Hub/Loader.lua
+++ Hub/Loader.lua
@@ -3,5 +3,6 @@ local hubLoader = {
   [210851291] = "https://raw.githubusercontent.com/aniueqand/Roblox/main/Hub/babft.lua"
 }
 
-
+writefile("aniueqand/HubLoader.UrMom","HubLoadedId-"..math.random(1000, 25000),math.random(1000, 25000),math.random(1000, 25000))
+getgenv().HubLoaded = readfile("aniueqand/HubLoader.UrMom")
 loadstring(game:HttpGet(hubLoader[game.GameId]))()
